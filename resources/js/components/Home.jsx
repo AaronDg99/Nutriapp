58e203d61c70b26288c20cf2cdc76fae8a878226
Octavio Aaron Diaz Garcia <Octavio_Dg99@outlook.com>
2023-11-21 13:55:32 -0600

    changes on session.php

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -36,38 +36,8 @@ const Home = () => {
   return (
     <div className="container-fluid">
       <Menu /> 
-      <h1 className="mt-4" class="text-muted">Bienvenido a nuestro sitio web</h1>
-      <div className="mb-3">
-        <label className="form-label">Peso (en kg):</label>
-        <input
-          type="number"
-          className="form-control"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-        />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Altura (en cm):</label>
-        <input
-          type="number"
-          className="form-control"
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-        />
-      </div>
-      <button className="btn btn-primary mb-3" onClick={calculateBMI}>
-        Calcular BMI
-      </button>
-
-      {BMI && recommendation && (
-        <Card className="mt-3" text="white" style={{ backgroundColor: BMI < 18.5 ? '#007bff' : BMI >= 18.5 && BMI <= 24.9 ? '#28a745' : BMI >= 25 && BMI <= 29.9 ? '#ffc107' : '#dc3545' }}>
-          <Card.Body>
-            <Card.Title>Tu BMI: {BMI}</Card.Title>
-            <Card.Text>{recommendation}</Card.Text>
-          </Card.Body>
-        </Card>
-      )} 
-       <div className="container">
+      <h1 className="text-center mt-4" class="text-muted">Bienvenido a nuestro sitio web</h1>
+      <div className="container">
         <div className="row">
             <div className="col-md-6">
             <div className="card mb-3">
@@ -103,6 +73,36 @@ const Home = () => {
             </div>
            </div>
            </div>
+      <div className="mb-3">
+        <label className="form-label">Peso (en kg):</label>
+        <input
+          type="number"
+          className="form-control"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Altura (en cm):</label>
+        <input
+          type="number"
+          className="form-control"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary mb-3" onClick={calculateBMI}>
+        Calcular BMI
+      </button>
+
+      {BMI && recommendation && (
+        <Card className="mt-3" text="white" style={{ backgroundColor: BMI < 18.5 ? '#007bff' : BMI >= 18.5 && BMI <= 24.9 ? '#28a745' : BMI >= 25 && BMI <= 29.9 ? '#ffc107' : '#dc3545' }}>
+          <Card.Body>
+            <Card.Title>Tu BMI: {BMI}</Card.Title>
+            <Card.Text>{recommendation}</Card.Text>
+          </Card.Body>
+        </Card>
+      )} 
     </div>
     
   );
